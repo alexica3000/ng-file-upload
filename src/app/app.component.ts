52f6b7d67fe40156aa43762136a0df8b9fd23e09
alex s <sanatateacom@gmail.com>
2020-11-03 13:07:44 +0200

    added button for upload files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'upload';
+  @ViewChild('fileInput') fileInput: ElementRef;
+  fileAttr = 'Choose file';
+
+  uploadFileEvt(imgFile: any): void {
+    if (imgFile.target.files && imgFile.target.files[0]) {
+      this.fileAttr = '';
+      Array.from(imgFile.target.files).forEach((file: File) => {
+        this.fileAttr += file.name + ' - ';
+      });
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = rs => {
+          const imgBase64Path = e.target.result;
+        };
+      };
+
+      reader.readAsDataURL(imgFile.target.files[0]);
+      this.fileInput.nativeElement.value = '';
+    } else {
+      this.fileAttr = 'Choose file';
+    }
+  }
 }
